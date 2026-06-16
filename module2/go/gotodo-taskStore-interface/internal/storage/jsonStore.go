3b5ea1d6954f8af077e2c9b7118d7d8a18623f01
pavlyysh <pgfu619@mail.ru>
2026-06-16 22:42:14 +0300

    start reaization of new gotodo-cli functionality

--- module2/go/gotodo-taskStore-interface/internal/storage/jsonStore.go
+++ module2/go/gotodo-taskStore-interface/internal/storage/jsonStore.go
@@ -1,1 +1,44 @@
 package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/Pavlyysh/Develp10/module2/go/gotodo-taskStore-interface/internal/models"
+)
+
+type JSONStore struct {
+	path string
+}
+
+func NewJSONStore() (*JSONStore, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("unable to get home dir: %w", err)
+	}
+
+	dir := filepath.Join(home, ".gotodo")
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, fmt.Errorf("failed create dir: %w", err)
+	}
+
+	return &JSONStore{
+		path: filepath.Join(dir, "tasks.json"),
+	}, nil
+}
+
+func (s *JSONStore) Load() ([]models.Task, error) {
+	file, err := os.OpenFile(s.path, os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("file open error: %w", err)
+	}
+	defer file.Close()
+
+	var tasks []models.Task
+	json.NewDecoder()
+}
+
+func (s *JSONStore) Save(tasks []models.Task) error {}
--- module2/go/gotodo-taskStore-interface/internal/storage/taskstore.go
+++ module2/go/gotodo-taskStore-interface/internal/storage/taskstore.go
@@ -1,1 +1,8 @@
 package storage
+
+import "github.com/Pavlyysh/Develp10/module2/go/gotodo-taskStore-interface/internal/models"
+
+type TaskStore interface {
+	Load() ([]models.Task, error)
+	Store(tasks []models.Task) error
+}
